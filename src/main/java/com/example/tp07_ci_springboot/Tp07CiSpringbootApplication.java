3a6096eae06300521c8f0450ea22a3b07d2a5176
ghozraneracha25 <ghozranerachouch@gmail.com>
2025-11-18 20:04:08 +0100

    Initial commit: Spring Boot project with CI configuration

---
+++ src/main/java/com/example/tp07_ci_springboot/Tp07CiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp07_ci_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp07CiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp07CiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp07_ci_springboot/Tp07CiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp07_ci_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp07CiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
